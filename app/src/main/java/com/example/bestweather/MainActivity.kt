51d49bfd6dad57a7da1dcbb35eb840c9d8281616
Ryan <rckennedy15@gmail.com>
2021-12-08 08:36:59 -0500

    initial commit

---
+++ app/src/main/java/com/example/bestweather/MainActivity.kt
@@ -0,0 +1,30 @@
+/*
+*   © Copyright Ryan Kennedy
+*
+*   This file is part of Best Weather.
+*
+*   Best Weather is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   Best Weather is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with Best Weather.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package com.example.bestweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
