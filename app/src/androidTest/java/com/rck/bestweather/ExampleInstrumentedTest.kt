0a927d04c0589a033cff7c27d7fb70b9aa6ac473
Ryan <rckennedy15@gmail.com>
2021-12-09 14:37:26 -0500

    add adaptive icon support

--- app/src/androidTest/java/com/rck/bestweather/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/rck/bestweather/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.bestweather
+package com.rck.bestweather
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.bestweather", appContext.packageName)
+        assertEquals("com.rck.bestweather", appContext.packageName)
     }
 }
--- app/src/test/java/com/rck/bestweather/ExampleUnitTest.kt
+++ app/src/test/java/com/rck/bestweather/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.bestweather
+package com.rck.bestweather
 
 import org.junit.Test
 
